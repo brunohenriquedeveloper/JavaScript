d65511b10c43f024da8cc4ff95b5083c9ec26430
Bruno Henrique <155691067+brunohenriquedeveloper@users.noreply.github.com>
2024-02-16 19:46:57 -0300

    Update index.js

--- aula13/index.js
+++ aula13/index.js
@@ -1,1 +1,22 @@
 // calculator program
+
+const display = document.getElementById("display");
+
+function appendtoDisplay(input) {
+    display.value += input;
+}
+
+function clearDiplay() {
+    display.value = "";
+}
+
+function calculate() {
+
+    try {
+        display.value = eval(display.value);
+    }
+    catch (error) {
+        display.value = "Error";
+    }
+    
+}
