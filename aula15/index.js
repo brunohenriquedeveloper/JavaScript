5de2b1c402a800cc3ded22933ee058ecf2681a85
Bruno Henrique <155691067+brunohenriquedeveloper@users.noreply.github.com>
2024-02-23 14:59:24 -0300

    full application

--- aula15/index.js
+++ aula15/index.js
@@ -10,3 +10,12 @@ function clearDisplay() {
     display.value = '';
 }
 
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+
+
+}
