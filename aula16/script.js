75896148b4930ee28724215c504b9a9772f05185
Bruno Henrique <155691067+brunohenriquedeveloper@users.noreply.github.com>
2024-03-04 19:33:24 -0300

    Update script.js

--- aula16/script.js
+++ aula16/script.js
@@ -1,3 +1,18 @@
 //calculator program
 
-const display = document.getElementsByClassName('')
+const display = document.getElementsByid('inp');
+
+function appendtoDisplay(input) {
+    display.value =+ input;
+}
+
+function clearDisplay(){
+    display.value = '';
+}
+
+function calculate() {
+    try { display.value = eval(display.value)
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
